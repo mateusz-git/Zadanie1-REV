6ece59af2176975cf8e6c91dfb27f16b9e283c72
mateusz-git <215851@edu.p.lodz.pl>
2020-10-13 14:43:26 +0200

    Implemented rev function

--- src/main/java/com/example/Zadanie1REV/Controller.java
+++ src/main/java/com/example/Zadanie1REV/Controller.java
@@ -1,7 +1,13 @@
 package com.example.Zadanie1REV;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    @GetMapping("/rev")
+    public String rev(@RequestParam String string) {
+        return new StringBuilder(string).reverse().toString();
+    }
 }
