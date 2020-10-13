887cc35ca35f1a43fbc9f74f97d57bcf959bfba5
mateusz-git <215851@edu.p.lodz.pl>
2020-10-13 14:31:15 +0200

    Initial commit

---
+++ src/main/java/com/example/Zadanie1REV/Zadanie1RevApplication.java
@@ -0,0 +1,13 @@
+package com.example.Zadanie1REV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie1RevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zadanie1RevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Zadanie1REV/Zadanie1RevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Zadanie1REV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie1RevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
